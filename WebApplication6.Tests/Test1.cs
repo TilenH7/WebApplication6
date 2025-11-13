420f25c1b75879753eec8007eec1436cfe157d3e
Tilen05 <tilen.hercog7@gmail.com>
2025-11-13 13:47:26 +0100

    Comit

---
+++ WebApplication6.Tests/Test1.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApplication6.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(false);
+
+        }
+    }
+}
---
+++ WebApplication6Tests/Test1.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication6Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.isTrue(true);
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.isTrue(false);
+        }
+    }
+}

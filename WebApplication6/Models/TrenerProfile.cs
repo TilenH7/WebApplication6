e1f28d7f0f4f639b6284fc6c1691af1b640c428c
andrejzelko <91586253+andrejzelko@users.noreply.github.com>
2025-12-10 14:15:52 +0100

    Ogled Trener profil

---
+++ WebApplication6/Models/TrenerProfile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace WebApplication6.Models
+{
+    public class TrenerProfile
+    {
+        public string Username { get; set; }      // "trener1"
+        public string Ime { get; set; }           // npr. "Marko Novak"
+        public string Opis { get; set; }          // opis trenerja
+        public string Specializacije { get; set; } // npr. "Crossfit, HIIT"
+        public string Lokacija { get; set; }      // npr. "Ljubljana"
+    }
+
+    public static class FakeTrenerDb
+    {
+        public static List<TrenerProfile> Trenerji { get; } = new()
+        {
+            new TrenerProfile
+            {
+                Username = "trener1",
+                Ime = "Marko Novak",
+                Opis = "Specialist za funkcionalne vadbe in individualno pripravo.",
+                Specializacije = "Funkcionalna vadba, HIIT, kondicijska priprava",
+                Lokacija = "Ljubljana"
+            }
+        };
+    }
+}
--- WebApplication6/Pages/Trener/Profil.cshtml.cs
+++ WebApplication6/Pages/Trener/Profil.cshtml.cs
@@ -2,21 +2,44 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using WebApplication6.Models;
 using System.Linq;
 using System.Collections.Generic;
+using System;
 
 namespace WebApplication6.Pages.Trener
 {
     public class ProfilModel : PageModel
     {
-        public string TrenerUsername { get; set; }
-        public List<OcenaTrenerja> Ocene { get; set; }
+        public TrenerProfile Trener { get; set; }
+        public double? PovprecnaOcena { get; set; }
+        public List<OcenaTrenerja> ZadnjeOcene { get; set; } = new();
+        public List<TerminVadbe> PrihajajociTermini { get; set; } = new();
 
         public void OnGet(string trener)
         {
-            TrenerUsername = trener;
-            Ocene = FakeOceneDb.Ocene
+            // osnovni podatki
+            Trener = FakeTrenerDb.Trenerji
+                .FirstOrDefault(t => t.Username == trener);
+
+            // ocene
+            var oceneTrenerja = FakeOceneDb.Ocene
                 .Where(o => o.TrenerUsername == trener)
+                .ToList();
+
+            if (oceneTrenerja.Any())
+            {
+                PovprecnaOcena = oceneTrenerja.Average(o => o.Ocena);
+                // zadnjih nekaj komentarjev (recimo 3)
+                ZadnjeOcene = oceneTrenerja
+                    .OrderByDescending(o => o.Id)
+                    .Take(3)
+                    .ToList();
+            }
+
+            // prihajajoèi termini
+            PrihajajociTermini = FakeTerminDb.Termini
+                .Where(t => t.TrenerUsername == trener &&
+                            t.DatumInCas >= DateTime.Now)
+                .OrderBy(t => t.DatumInCas)
                 .ToList();
         }
     }
 }
-
